f5a45e4680aad4bd4e62d2db99ab685b063cd785
iyahoo <s1200191@gmail.com>
2018-01-14 07:06:53 +0900

    :wrench: [Fix]

--- inits/30_whitespace.el
+++ inits/30_whitespace.el
@@ -2,14 +2,9 @@
   :bind ("C-x w" . global-whitespace-mode)
   :init (global-whitespace-mode t)
   :config
-  (setq whitespace-style '(face tabs tab-mark spaces space-mark)
-        whitespace-display-mappings '((space-mark ?\u3000 [?\u25a1])
-                                      (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t]))
-        whitespace-space-regexp "\\(\u3000+\\)")
+  (setq whitespace-style '(face tabs tab-mark)
+        whitespace-display-mappings '((tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
   (set-face-foreground 'whitespace-tab "#00aaaa")
   (set-face-background 'whitespace-tab 'nil)
   (set-face-underline  'whitespace-tab 'nil)
-  (set-face-foreground 'whitespace-space "#7cfc00")
-
-  (set-face-background 'whitespace-space 'nil)
-  (set-face-bold-p 'whitespace-space t))
+  (set-face-bold-p 'whitespace-space nil))
