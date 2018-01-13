344fee484807c62b7c1d7cadd3e3b0d8cfa45a5e
iyahoo <s1200191@gmail.com>
2018-01-14 00:01:06 +0900

    :hammer: [Fix] to use setq-default

--- inits/15_anzu.el
+++ inits/15_anzu.el
@@ -3,9 +3,8 @@
   :bind (("M-%" . anzu-query-replace)
          ("C-M-%" . anzu-query-replace-regexp))
   :config
-  (custom-set-variables
-   '(anzu-mode-lighter "")
-   '(anzu-deactivate-region t)
-   '(anzu-search-threshold 1000)
-   '(anzu-replace-to-string-separator " => ")))
+  (setq-default anzu-mode-lighter ""
+                anzu-deactivate-region t
+                anzu-search-threshold 1000
+                anzu-replace-to-string-separator " => "))
 
