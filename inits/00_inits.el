d516c16ed5271d9afd456d144b01671d39bd0549
iyahoo <s1200191@gmail.com>
2018-01-13 23:31:32 +0900

    :bug: [Remove] surplus custom setting

--- inits/00_inits.el
+++ inits/00_inits.el
@@ -66,19 +66,3 @@
 ;; Start server
 (use-package server
   :init (server-start))
-
-;; Custom set (Auto inserted)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(quack-pltish-defn-face ((t (:foreground "DarkGoldenrod2" :weight bold))))
-  '(quack-pltish-module-defn-face ((t (:inherit quack-pltish-defn-face :foreground "orange2"))))
-  '(quack-pltish-paren-face ((t (:foreground "salmon"))))
-  '(quack-pltish-selfeval-face ((t (:foreground "indian red"))))))
