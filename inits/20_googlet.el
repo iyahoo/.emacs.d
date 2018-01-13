5ed51e415eecd26784bd364b4261f14f7eb0c534
iyahoo <s1200191@gmail.com>
2018-01-13 23:44:14 +0900

    :bug: [Fix] keybind and default setting

--- inits/20_googlet.el
+++ inits/20_googlet.el
@@ -1,11 +1,8 @@
 (use-package google-translate
   :bind (("C-x C-t"     . google-translate-at-point)
-         ;; ("C-x C-T" . google-translate-query-translate)
+         ("C-x C-M-t" . google-translate-query-translate)
          ("C-x C-r"     . google-translate-at-point-reverse)
-         ;; ("C-x C-R" . google-translate-query-translate-reverse)
-         )
-  :config
-  (custom-set-variables
-   '(google-translate-default-source-language "en")
-   '(google-translate-default-target-language "ja")))
-
+         ("C-x C-M-r" . google-translate-query-translate-reverse))
+  :init
+  (setq-default google-translate-default-source-language "en"
+                google-translate-default-target-language "ja"))
