05a96698ea2a83f46c6b9864fe89c2c7e1101821
iyahoo <s1200191@gmail.com>
2018-01-15 14:25:58 +0900

    :wrench: [Disable]

--- inits/15_mainline.el
+++ inits/15_mainline.el
@@ -1,6 +1,9 @@
-;;;
-
 (use-package main-line
+  :disabled t
   :config
   (setq main-line-separator-style 'arrow)
   (defmain-line row "%4l"))
+
+(use-package powerline
+  :disabled t
+  :init (powerline-default-theme))
