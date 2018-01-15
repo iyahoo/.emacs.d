c808f1037794d4e6ea5386ca0b0eb54f41ed74c2
iyahoo <s1200191@gmail.com>
2018-01-15 16:59:11 +0900

    :hammer: [Fix] useing use-package

--- inits/30_coq.el
+++ inits/30_coq.el
@@ -1,13 +1,4 @@
-;; (load-file "/usr/local/share/emacs/site-lisp/proof-general/generic/proof-site.el")
-
-
-;; (add-hook 'coq-mode-hook
-;;           (lambda ()
-;;             (define-coq-indent
-;;               (Case 4)
-;;               (SCase 2)
-;;               (SSCase 2))))
-
 (load "~/.emacs.d/lisp/PG/generic/proof-site")
-(setq auto-mode-alist (cons '("\\.v$" . coq-mode) auto-mode-alist))
-(autoload 'coq-mode "coq" "Major mode for editing Coq vernacular." t)
+
+(use-package coq
+  :mode ("\\.v$" . coq-mode))
