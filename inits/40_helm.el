958bd3784249e09a6ddd4bf742cdc059dfdcac6d
iyahoo <s1200191@gmail.com>
2018-01-15 11:55:09 +0900

    :wrench: [Fix]

--- inits/40_helm.el
+++ inits/40_helm.el
@@ -1,17 +1,3 @@
-(defalias 'hg 'helm-do-grep)
-(defalias 'ht 'helm-etags-select)
-(defalias 'ha 'helm-ag)
-(defalias 'haf 'helm-ag-this-file)
-
-(bind-keys* ("M-y" . helm-show-kill-ring)
-            ("M-x" . helm-M-x)
-            ("C-o" . helm-swoop)
-            ("C--" . helm-company)
-            ("C-c o" . helm-multi-swoop)
-            ("C-c h" . helm-mini)
-            ("C-c -" . helm-c-yas-complete)
-            ("C-c s" . helm-imenu))
-
 (use-package helm-mode
   :commands (helm-occur-from-isearch
              helm-show-kill-ring
@@ -25,4 +11,18 @@
              helm-do-grep
              helm-etags-select
              helm-ag
-             helm-ag-this-file))
+             helm-ag-this-file
+             helm-find-files)
+  :init
+  (bind-keys* ("M-y" . helm-show-kill-ring)
+              ("M-x" . helm-M-x)
+              ("C-o" . helm-swoop)
+              ("C--" . helm-company)
+              ("C-c o" . helm-multi-swoop)
+              ("C-c h" . helm-mini)
+              ("C-c s" . helm-imenu)
+              ("C-x C-f" . helm-find-files))
+  (defalias 'hg 'helm-do-grep)
+  (defalias 'ht 'helm-etags-select)
+  (defalias 'ha 'helm-ag)
+  (defalias 'haf 'helm-ag-this-file))
