74b6fb21637dbc753e104fea63310e4c490cd1a4
iyahoo <s1200191@gmail.com>
2018-01-15 17:31:34 +0900

    :lipstick: [Remove] whitespace

--- inits/30_matlab.el
+++ inits/30_matlab.el
@@ -8,10 +8,10 @@
   :mode (("\\.m\\'" . matlab-mode))
   :config
   (setq matlab-shell-command "/Applications/MATLAB_R2017a.app/bin/matlab"
-        matlab-shell-command-swithes '("-nojvm -nodisplay -nosplash")  
+        matlab-shell-command-swithes '("-nojvm -nodisplay -nosplash")
         matlab-indent-level 4
         matlab-indent-function-body nil
-        matlab-highlight-cross-function-variables t  
+        matlab-highlight-cross-function-variables t
         matlab-return-add-semicolon nil
         matlab-case-level '(4 . 4)
         matlab-fill-fudge-hard-maximum 100
