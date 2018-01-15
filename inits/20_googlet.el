666608585800c130377a19a7f2daeac20dd54f8c
iyahoo <s1200191@gmail.com>
2018-01-15 17:31:53 +0900

    :wrench: [Fix] key binds to global

--- inits/20_googlet.el
+++ inits/20_googlet.el
@@ -1,8 +1,8 @@
 (use-package google-translate
-  :bind (("C-x C-t"     . google-translate-at-point)
-         ("C-x C-M-t" . google-translate-query-translate)
-         ("C-x C-r"     . google-translate-at-point-reverse)
-         ("C-x C-M-r" . google-translate-query-translate-reverse))
+  :bind* (("C-x C-t"     . google-translate-at-point)
+          ("C-x C-M-t" . google-translate-query-translate)
+          ("C-x C-r"     . google-translate-at-point-reverse)
+          ("C-x C-M-r" . google-translate-query-translate-reverse))
   :init
   (setq-default google-translate-default-source-language "en"
                 google-translate-default-target-language "ja"))
