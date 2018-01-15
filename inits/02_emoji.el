7f866ea706b4729c9a11e306611b0ceef924b8d7
iyahoo <s1200191@gmail.com>
2018-01-15 11:55:30 +0900

    :wrench: [Fix] hook for magit

--- inits/02_emoji.el
+++ inits/02_emoji.el
@@ -2,7 +2,9 @@
   :config
   (defalias 'emoji-sheet 'emoji-cheat-sheet-plus-insert)
   (defalias 'emoji 'emojify-mode)
-  (add-hook 'magit-mode-hook 'emojify-mode))
+  (add-hook 'magit-mode-hook 'emojify-mode)
+  (add-hook 'text-mode-hook 'emojify-mode)
+  (add-hook 'markdown-mode-hook 'emojify-mode))
 
 
 
