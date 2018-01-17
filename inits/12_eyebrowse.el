a636b2d2d298beb1938e8c27daaa5a498e0ddc2a
iyahoo <s1200191@gmail.com>
2018-01-17 16:35:39 +0900

    :wrench: [Fix] keybind and default buffer to *scratch*

--- inits/12_eyebrowse.el
+++ inits/12_eyebrowse.el
@@ -3,10 +3,21 @@
   :defer nil
   :bind* (([C-tab]   . eyebrowse-next-window-config)
           ([C-S-tab] . eyebrowse-prev-window-config)
-          ("C-c w d" . eyebrowse-close-window-config))
+          ("M-m c" . eyebrowse-create-window-config)
+          ("M-m d" . eyebrowse-close-window-config))
   :init
   (setq-default eyebrowse-keymap-prefix (kbd "C-c w"))
   (eyebrowse-mode t)
   :config
   (add-to-list 'window-persistent-parameters '(window-side . writable))
-  (add-to-list 'window-persistent-parameters '(window-slot . writable)))
+  (add-to-list 'window-persistent-parameters '(window-slot . writable))
+  (advice-add 'eyebrowse-create-window-config :after (lambda () (switch-to-buffer "*scratch*"))))
+
+(use-package desktop
+  :disabled
+  :init
+  (desktop-save-mode t)
+  (push (expand-file-name "desktops/" user-emacs-directory) desktop-path)
+  (setq desktop-auto-save-timeout 60)
+  (push 'kill-ring desktop-globals-to-save))
+
