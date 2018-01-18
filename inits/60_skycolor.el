b8e425fafa88e7eff704705186f5930a424c15bc
iyahoo <s1200191@gmail.com>
2018-01-19 01:39:40 +0900

    :wrench: [Fix]

--- inits/60_skycolor.el
+++ inits/60_skycolor.el
@@ -8,10 +8,16 @@
     (progn (push '(:eval (sky-color-clock)) mode-line-format)
            (setq sky-color-clock-mode t))))
 
+(defun my-file-contents (filename)
+  "Return the contents of FILENAME as string."
+  (remove ?\n (with-temp-buffer
+                (insert-file-contents filename)
+                (buffer-string))))
+
 (use-package sky-color-clock
   :defer nil
   :init
   (require 'sky-color-clock)
   (sky-color-clock-initialize 35)
-  (sky-color-clock-initialize-openweathermap-client "d2f125b02364f09e899118e8738a93d8" 2112615)
+  (sky-color-clock-initialize-openweathermap-client (my-file-contents "60_sky_api.el") 2112615)
   (setq sky-color-clock-enable-emoji-icon t))
