95cac50dda4d4aaee83cb88acf3fc1c96d6aa0c7
iyahoo <s1200191@gmail.com>
2018-01-18 16:18:49 +0900

    :wrench: [Add] initial state of new window

--- inits/12_eyebrowse.el
+++ inits/12_eyebrowse.el
@@ -1,17 +1,15 @@
 ;; Alternative of elscreen
 (use-package eyebrowse
-  :defer nil
   :bind* (([C-tab]   . eyebrowse-next-window-config)
           ([C-S-tab] . eyebrowse-prev-window-config)
           ("M-m c" . eyebrowse-create-window-config)
           ("M-m d" . eyebrowse-close-window-config))
   :init
-  (setq-default eyebrowse-keymap-prefix (kbd "C-c w"))
-  (eyebrowse-mode t)
+  (setq-default eyebrowse-keymap-prefix (kbd "C-c w")
+                eyebrowse-new-workspace t
+                eyebrowse-close-window-config-prompt t)
   :config
-  (add-to-list 'window-persistent-parameters '(window-side . writable))
-  (add-to-list 'window-persistent-parameters '(window-slot . writable))
-  (advice-add 'eyebrowse-create-window-config :after (lambda () (switch-to-buffer "*scratch*"))))
+  (eyebrowse-mode t))
 
 (use-package desktop
   :disabled
@@ -20,4 +18,3 @@
   (push (expand-file-name "desktops/" user-emacs-directory) desktop-path)
   (setq desktop-auto-save-timeout 60)
   (push 'kill-ring desktop-globals-to-save))
-
