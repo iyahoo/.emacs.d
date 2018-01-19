73e5b8d82ace706c0dd39150152750ae14dd2585
iyahoo <s1200191@gmail.com>
2018-01-19 15:26:42 +0900

    :wrench: [Fix] disable

--- inits/60_skycolor.el
+++ inits/60_skycolor.el
@@ -9,15 +9,23 @@
            (setq sky-color-clock-mode t))))
 
 (defun my-file-contents (filename)
-  "Return the contents of FILENAME as string."
-  (remove ?\n (with-temp-buffer
-                (insert-file-contents filename)
-                (buffer-string))))
+  "Return the contents of FILENAME as string removing ?\n."
+  (remove ?\n
+          (with-temp-buffer
+            (insert-file-contents filename)
+            (buffer-string))))
+
+(defun expand-emacs-home (path)
+  (expand-file-name path user-emacs-directory))
+
+(defconst sky-api-key
+  (my-file-contents (expand-emacs-home "inits/___sky_api.el")))
 
 (use-package sky-color-clock
+  :disabled
   :defer nil
   :init
   (require 'sky-color-clock)
   (sky-color-clock-initialize 35)
-  (sky-color-clock-initialize-openweathermap-client (my-file-contents "60_sky_api.el") 2112615)
+  (sky-color-clock-initialize-openweathermap-client sky-api-key 2112615)
   (setq sky-color-clock-enable-emoji-icon t))
