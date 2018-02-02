7c88afd302f62a7e521dc9046863ee365103f691
iyahoo <s1200191@gmail.com>
2018-02-02 13:51:04 +0900

    :wrench: [Remove] yatex

--- inits/30_tex.el
+++ inits/30_tex.el
@@ -6,6 +6,4 @@
    ((eq system-type 'gnu/linux)
     (setq dvi2-command "evince"))
    ((eq system-type 'darwin)
-    (setq dvi2-command "open -a Preview")))
-  (add-hook 'yatex-mode-hook
-            '(lambda () (setq auto-fill-function nil))))
+    (setq dvi2-command "open -a Preview"))))
