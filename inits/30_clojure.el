43def2d19ca722f5cb5714c1f4064a2f4947f86c
iyahoo <s1200191@gmail.com>
2018-06-13 16:17:44 +0900

    :wrench: [Fix] match macro indent

--- inits/30_clojure.el
+++ inits/30_clojure.el
@@ -14,9 +14,9 @@
   (add-hook 'cider-mode-hook #'company-mode)
   (add-hook 'cider-mode-hook #'eldoc-mode)
   (add-hook 'cider-repl-mode-hook #'company-mode)
-  (add-hook 'cider-repl-mode-hook #'eldoc-mode)    
+  (add-hook 'cider-repl-mode-hook #'eldoc-mode)
   :diminish subword-mode
-  :config  
+  :config
   (setq nrepl-log-messages t
         cider-repl-display-in-current-window t
         cider-repl-use-clojure-font-lock t
@@ -24,5 +24,5 @@
         cider-font-lock-dynamically '(macro core function var)
         cider-overlays-use-font-lock t)
   (cider-repl-toggle-pretty-printing)
-  (bind-key "M-r" 'save-relode-test cider-mode-map))
-
+  (bind-key "M-r" 'save-relode-test cider-mode-map)
+  (put-clojure-indent 'match 1))
