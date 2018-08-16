4cd964ae4e36d94694102fba722ef9b69d2afd1f
iyahoo <s1200191@gmail.com>
2018-08-16 11:59:24 +0900

    :bug: [Fix]

--- inits/15_hideshow.el
+++ inits/15_hideshow.el
@@ -1,4 +1,5 @@
-(use-package hs
+(use-package hideshow
   :bind
-  (("C-M-h" . 'hs-toggle-hiding)
-   ("M-m C-M-h" . 'hs-hide-all)))
+  (("C-M-h" . hs-toggle-hiding)
+   ("M-m C-M-h" . hs-hide-all)))
+
