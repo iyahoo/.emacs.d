e267029da4e1569ba9f7e976453bb535e677ec62
iyahoo <s1200191@gmail.com>
2018-08-16 11:58:22 +0900

    :sparkles: [Add] git-mode in mode-line

--- inits/06_magit.el
+++ inits/06_magit.el
@@ -3,3 +3,20 @@
   (defalias 'ms 'magit-status)
   :config
   (setq magit-auto-revert-mode nil))
+
+;; Show Git branch information to mode-line
+(defun my/update-git-branch-mode-line ()
+  (let* ((branch (replace-regexp-in-string
+                  "[\r\n]+\\'" ""
+                  (shell-command-to-string "git symbolic-ref -q HEAD")))
+         (mode-line-str (if (string-match "^refs/heads/" branch)
+                            (format "[%s]" (substring branch 11))
+                          "[Not Repo]")))
+    (propertize mode-line-str
+                'face '((:foreground "Dark green" :weight bold)))))
+
+(let ((cell (or (memq 'mode-line-position mode-line-format)
+                (memq 'mode-line-buffer-identification mode-line-format)))
+      (newcdr '(:eval (my/update-git-branch-mode-line))))
+  (unless (member newcdr mode-line-format)
+    (setcdr cell (cons newcdr (cdr cell)))))
