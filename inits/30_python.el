924470b8b580534205a8fac455262ea275b4d742
iyahoo <s1200191@gmail.com>
2018-09-14 12:23:03 +0900

    :sparkles: [Add] conf

--- inits/30_python.el
+++ inits/30_python.el
@@ -5,9 +5,14 @@
   (setq jedi:complete-on-dot t))
 
 (use-package python-mode
-  :mode (("\\.py\\'" . python-mode)))
+  :mode (("\\.py\\'" . python-mode))
+  :init
+  (setq python-shell-interpreter "python3"
+        python-shell-interpreter-args "-i")
+  (add-hook 'python-mode-hook 'py-yapf-enable-on-save))
 
-(use-package virtualenvwrapper)
+(use-package virtualenvwrapper :disabled)
 (use-package auto-virtualenvwrapper
+  :disabled
   :init
   (add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate))
